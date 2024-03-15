405413d250282dbc68eca0ef2957cfc86fdb5498
Joselay <tongmenglaysmae5@gmail.com>
2024-03-15 17:34:16 +0700

    first commit

---
+++ src/features/cart/Cart.jsx
@@ -0,0 +1,43 @@
+import { useDispatch, useSelector } from "react-redux";
+import LinkButton from "../../ui/LinkButton";
+import Button from "../../ui/Button";
+import CartItem from "./CartItem";
+import { clearCart, getCart } from "./cartSlice";
+import { getUsername } from "../user/userSlice";
+import EmptyCart from "./EmptyCart";
+
+function Cart() {
+  const dispatch = useDispatch();
+  const username = useSelector(getUsername);
+  const cart = useSelector(getCart);
+
+  function handleClearCart() {
+    dispatch(clearCart());
+  }
+
+  if (!cart.length) return <EmptyCart />;
+
+  return (
+    <div className="px-4 py-3">
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
+
+      <h2 className="text-xl font-semibold mt-7">Your cart, {username}</h2>
+      <ul className="mt-3 border-b divide-y divide-stone-200">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.pizzaId} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
+        <Button to="/order/new" type="primary">
+          Order pizzas
+        </Button>
+        <Button onClick={handleClearCart} type="secondary">
+          Clear cart
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
